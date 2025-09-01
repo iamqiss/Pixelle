e3b5e356e2633d9473ad4fc31269141eb77ccf0c
ngomaneneo24-hub <ngomaneneo24@gmail.com>
2025-09-01 21:13:02 +0000

    rebrand to density

--- third_party/opensearch_backup_20250901205951/server/src/main/java/org/opensearch/common/cache/stats/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The Density Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-/** A package for cache stats. */
-package org.density.common.cache.stats;
